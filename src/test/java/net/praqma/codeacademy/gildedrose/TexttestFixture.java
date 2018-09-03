64539d931d95231fea5e49bc0867b05f5021d9a5
ohad18 <42245832+ohad18@users.noreply.github.com>
2018-09-03 17:25:22 +0000

    Update TexttestFixture.java

--- src/test/java/net/praqma/codeacademy/gildedrose/TexttestFixture.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/TexttestFixture.java
@@ -1,6 +1,6 @@
 package net.praqma.codeacademy.gildedrose;
 
-ublic class TexttestFixture {
+public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
